567a706cd3ef3d1745795b5a4a1d16344bad29a1
Yheuda12 <y0533005396@gmail.com>
2022-05-29 12:05:11 +0300

    new commit

---
+++ type_Var.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 29 11:44:12 2022
+
+@author: IMOE001
+"""
+
+## int .....-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6....
+int_num = 6
+print(int_num)
+int_num=100049473
+print(int_num)
+print(type(int_num))
+
+## float ... -6.2,-5.343,-4.25,........,0,1.3,4.5.....
+float_num = 6.6
+print(float_num)
+float_num=100049473.3376
+print(float_num)
+print(type(float_num))
+
+## boolean  True, False
+flag = True
+print(flag)
+flag=False
+print(flag)
+print(type(flag))
+
+## str-string  "barak", "Hila" 
+string = "Hello world"
+print(string)
+string="Hila"
+print(string)
+print(type(string))
